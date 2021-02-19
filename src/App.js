5035ab808a5666a1fadc204c743bca5df534cf31
Mustafa Syahmi <mussyahmi31@gmail.com>
2021-02-19 10:23:37 +0800

    Header + assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Clever Programmer</h1>
+      <Header />
+      {/* items => snapping */}
+      {/* items => snapping */}
+      {/* items => snapping */}
+      {/* items => snapping */}
     </div>
   );
 }
